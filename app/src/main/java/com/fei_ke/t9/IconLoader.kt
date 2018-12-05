a7f6f514d47ac3b73aab3f2fc58d7bd78fc5490a
fei-ke <jinyang656@qq.com>
2018-12-06 00:29:56 +0800

    Use Thread load icon

--- app/src/main/java/com/fei_ke/t9/IconLoader.kt
+++ app/src/main/java/com/fei_ke/t9/IconLoader.kt
@@ -3,12 +3,15 @@ package com.fei_ke.t9
 import android.content.ComponentName
 import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
-import android.os.AsyncTask
 import android.widget.ImageView
 import java.lang.ref.WeakReference
+import java.util.*
+import java.util.concurrent.Executors
+import java.util.concurrent.ThreadPoolExecutor
 
 object IconLoader {
     private val cache = HashMap<String, Drawable>()
+    private val iconLoaderPool = Executors.newCachedThreadPool() as ThreadPoolExecutor
 
     fun load(imageView: ImageView, pkgName: String, className: String) {
         val cacheKey = getTag(pkgName, className)
@@ -17,42 +20,38 @@ object IconLoader {
             imageView.setImageDrawable(cache[cacheKey])
         } else {
             imageView.setImageDrawable(null)
-            LoadIconTask(imageView, pkgName, className).execute()
+            iconLoaderPool.execute(LoadIconTask(imageView, pkgName, className))
         }
     }
 
     private fun getTag(pkgName: String, className: String) = "$pkgName:$className"
 
-    private class LoadIconTask(imageView: ImageView, val pkgName: String, val className: String) :
-        AsyncTask<Unit, Int, Drawable?>() {
-
+    private class LoadIconTask(imageView: ImageView, val pkgName: String, val className: String) : Runnable {
         private val imageViewRef = WeakReference(imageView)
 
         init {
             imageView.setTag(R.id.key_load_icon, getTag(pkgName, className))
         }
 
-        override fun doInBackground(vararg params: Unit?): Drawable? {
-            return try {
-                App.instance.packageManager.getActivityIcon(
-                    ComponentName(pkgName, className)
-                )
+        override fun run() {
+            val icon = try {
+                App.instance.packageManager.getActivityIcon(ComponentName(pkgName, className))
             } catch (e: PackageManager.NameNotFoundException) {
                 ShortcutLoader.notifyPackageRemoved(pkgName)
                 null
-            }
-
-        }
-
-        override fun onPostExecute(result: Drawable?) {
-            result ?: return
+            } ?: return
 
             val tagOrCacheKey = getTag(pkgName, className)
-            cache[tagOrCacheKey] = result
-
-            imageViewRef.get()
-                ?.takeIf { it.getTag(R.id.key_load_icon) == tagOrCacheKey }
-                ?.apply { setImageDrawable(result) }
+            cache[tagOrCacheKey] = icon
+            imageViewRef.get()?.apply {
+                post {
+                    if (getTag(R.id.key_load_icon) == tagOrCacheKey) {
+                        alpha = 0f
+                        setImageDrawable(icon)
+                        animate().alpha(1f).setDuration(400).start()
+                    }
+                }
+            }
         }
     }
 }
