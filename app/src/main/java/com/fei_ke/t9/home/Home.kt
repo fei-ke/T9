3b7cfc5eb38ed4362f27a156f271a1726cf2ba9a
fei-ke <jinyang656@gmail.com>
2023-04-08 14:19:49 +0800

    Add app info

--- app/src/main/java/com/fei_ke/t9/home/Home.kt
+++ app/src/main/java/com/fei_ke/t9/home/Home.kt
@@ -7,12 +7,14 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
+import android.net.Uri
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,6 +24,8 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -36,14 +40,17 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.fei_ke.t9.R
 import com.fei_ke.t9.Shortcut
 import com.fei_ke.t9.widget.LifecycleAware
 import com.google.accompanist.drawablepainter.rememberDrawablePainter
@@ -109,6 +116,8 @@ fun AppList(appList: List<Shortcut>) {
 
 @Composable
 private fun AppShortcut(context: Context, item: Shortcut) {
+    var showMenu by remember { mutableStateOf(false) }
+
     Box(
         modifier = Modifier
             .padding(8.dp),
@@ -124,9 +133,14 @@ private fun AppShortcut(context: Context, item: Shortcut) {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .combinedClickable(onClick = {
-                        startApp(context, item)
-                    })
+                    .combinedClickable(
+                        onClick = {
+                            startApp(context, item)
+                        },
+                        onLongClick = {
+                            showMenu = true
+                        }
+                    )
                     .padding(8.dp)
 
             ) {
@@ -145,7 +159,18 @@ private fun AppShortcut(context: Context, item: Shortcut) {
                 )
             }
         }
-
+        DropdownMenu(expanded = showMenu, onDismissRequest = { showMenu = false }) {
+            DropdownMenuItem(
+                modifier = Modifier.defaultMinSize(minWidth = 160.dp),
+                text = {
+                    Text(text = stringResource(id = R.string.app_info))
+                },
+                onClick = {
+                    startAppInfo(context, item)
+                    showMenu = false
+                }
+            )
+        }
     }
 }
 
@@ -162,6 +187,14 @@ fun startApp(context: Context, shortcut: Shortcut) {
     context.startActivity(intent)
 }
 
+fun startAppInfo(context: Context, shortcut: Shortcut) {
+    val intent = Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+        addCategory(Intent.CATEGORY_DEFAULT)
+        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        data = Uri.parse("package:${shortcut.pkgName}")
+    }
+    context.startActivity(intent)
+}
 
 @Composable
 private fun Panel(
